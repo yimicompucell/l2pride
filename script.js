cf8b86cf28e53f75b131eda5c0fcf8cbc58b7418
Nickson Valera <72787086+yimicompucell@users.noreply.github.com>
2023-10-11 00:17:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 const content = document.querySelector('.content'); // Selecciona el elemento con el fondo semitransparente
+const body = document.body;
 
 menuToggle.addEventListener('click', () => {
     if (menu.classList.contains('show-menu')) {
@@ -18,12 +19,14 @@ menuToggle.addEventListener('click', () => {
         menu.classList.remove('show-menu');
         content.style.opacity = 0; // Oculta el fondo
         content.style.pointerEvents = 'none'; // Deshabilita interacción con el fondo
+        body.style.overflow = 'auto'; // Habilita el desplazamiento
     } else {
         // Si el menú está cerrado, ábrelo
         menu.style.height = '100%';
         menu.classList.add('show-menu');
         content.style.opacity = 0.5; // Muestra el fondo con opacidad del 50%
         content.style.pointerEvents = 'auto'; // Habilita interacción con el fondo
+        body.style.overflow = 'hidden'; // Deshabilita el desplazamiento
     }
 });
 
@@ -34,4 +37,5 @@ closeButton.addEventListener('click', () => {
     menu.classList.remove('show-menu');
     content.style.opacity = 0; // Oculta el fondo nuevamente
     content.style.pointerEvents = 'none'; // Deshabilita interacción con el contenido principal
+    body.style.overflow = 'auto'; // Habilita el desplazamiento
 });
