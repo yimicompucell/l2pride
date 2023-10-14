86ee865baee7ca49410239917bcd175b9598a0b2
Nickson Valera <72787086+yimicompucell@users.noreply.github.com>
2023-10-14 18:27:07 -0500

    gg

--- scriptlibro.js
+++ scriptlibro.js
@@ -47,3 +47,28 @@ function prevChapter() {
 
 // Mostrar el primer capítulo al cargar la página
 showCurrentChapter();
+
+const comentarioForm = document.getElementById('comentario-form');
+comentarioForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const comentario = document.getElementById('comentario').value;
+    const calificacion = document.getElementById('calificacion').value;
+
+    // Aquí puedes hacer algo con los datos del comentario, como mostrarlos en la página o enviarlos a un servidor.
+    // Por ejemplo, podrías crear elementos HTML y agregar los comentarios al DOM.
+
+    // Ejemplo de cómo mostrar un comentario en la página:
+    const comentariosContainer = document.querySelector('.comentarios-resenas');
+    const comentarioElement = document.createElement('div');
+    comentarioElement.innerHTML = `
+        <p><strong>${nombre}:</strong></p>
+        <p>Calificación: ${calificacion} estrellas</p>
+        <p>${comentario}</p>
+    `;
+    comentariosContainer.appendChild(comentarioElement);
+
+    // Limpia el formulario después de enviar el comentario
+    comentarioForm.reset();
+});
+
